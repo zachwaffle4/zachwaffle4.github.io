6c08d0ed72c8d93930b39755a6ed4adb35cc4889
Zach Harel <zach@zharel.me>
2025-10-17 17:54:20 -0400

    update metadata to reflect personal branding and portfolio title

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,15 +11,15 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Zachary Harel',
+    template: '%s | Zachary Harel',
   },
   description: 'This is my portfolio.',
   openGraph: {
-    title: 'My Portfolio',
+    title: 'Zachary Harel',
     description: 'This is my portfolio.',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'Zachary Harel',
     locale: 'en_US',
     type: 'website',
   },
